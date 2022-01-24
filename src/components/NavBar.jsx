5d74b09d948c5c2efb99644bd6d1adcce3109896
AlejandroT117 <ualejandrotrejo@gmail.com>
2022-01-24 13:19:06 -0600

    NavBar initiated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,16 @@
-const 
+const NavBar = () =>{
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>Bicicletas de ruta</li>
+        <li>Bicicletas de montaña</li>
+        <li>Bicicletas urbanas</li>
+        <li>Rodillos</li>
+        <li>Refacciones</li>
+      </ul>
+    </>
+  )
+}
+
+export default NavBar;
